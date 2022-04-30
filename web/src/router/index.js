458b2faefaf2b96b13cee537acff59bbed42a725
A1an <387805107@qq.com>
2022-05-01 02:05:47 +0800

    路由模式由createWebHistory变更为createWebHistory来禁用地址栏中的#号

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,24 +1,24 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [{
   path: '/',
-  redirect: '/login'
+  redirect: '/login',
 },
 {
   path: '/init',
   name: 'Init',
-  component: () => import('@/view/init/index.vue')
+  component: () => import('@/view/init/index.vue'),
 },
 {
   path: '/login',
   name: 'Login',
-  component: () => import('@/view/login/index.vue')
-}
+  component: () => import('@/view/login/index.vue'),
+},
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
